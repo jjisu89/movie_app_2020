48d841f90f0c622f2775bd0b4520d47aee1ac2fb
jisu_jeong <jisujeong@jisu-jeongui-MacBookPro.local>
2021-10-11 23:52:11 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        Contact: (jji_su89@naver.coom).replace('_','')
+      </span>
+    </div>
+  );
+}
+
+export default About;
